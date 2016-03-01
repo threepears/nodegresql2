7160d6c5eb0a797cc4ac4a4fd3e11746a5a7e5c8
Michael Conrad <michael@threepears.com>
2016-03-01 10:51:05 -0600

    Initial commit, creating basic routes for artist, genre, mediatype, and playlist

---
+++ server.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const express = require("express");
+
+const models = require("./models/");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const Genre = require("./models/").Genre;
+const MediaTypes = require("./models/")
+
+app.get("/", (req, res) => {
+  res.send({
+    status: "Success"
+  });
+});
+
+app.get("/genres", (req, res) => {
+  models.Genre.findAll().then((genres) => {
+    res.send(genres);
+  });
+});
+
+app.get('/mediatypes', (req, res) => {
+  models.MediaType.findAll().then((mediatypes) => {
+    res.send(mediatypes);
+  });
+});
+
+app.get("/artists", (req, res) => {
+  models.Artist.findAll().then((artists) => {
+    res.send(artists);
+  });
+});
+
+app.get("/playlists", (req, res) => {
+  models.Playlist.findAll().then((playlists) => {
+    res.send(playlists);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
